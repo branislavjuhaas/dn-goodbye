40e20714f66fe178e8ea4c0ed5424e3e687b5a1b
Branislav Juhás <juhas.branislaw@gmail.com>
2026-04-10 20:12:32 +0200

    Revert "feat: add NumberFlow component for animated countdown and refactor AppCountdownHero"
    This reverts commit cbc36639c8a4cbf427aa5e03e30a4c344652ccbb.

--- app/components/AppCountdownHero.vue
+++ app/components/AppCountdownHero.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-import NumberFlow, { continuous } from "@number-flow/vue";
-const props = defineProps<{
-  remaining: number;
+defineProps<{
+  display: number;
+  progressBarValue: number;
   nameDay: string;
 }>();
 
 const { t } = useI18n();
-
-const display = ref(950);
-
-onMounted(() => {
-  setTimeout(() => {
-    display.value = props.remaining;
-  }, 500);
-});
 </script>
 
 <template>
@@ -23,26 +15,19 @@ onMounted(() => {
     </p>
     <h1
       class="mt-2 text-4xl sm:text-5xl font-bold text-highlighted text-center text-balance">
-      <NumberFlow
-        :value="display"
-        :spin-timing="{ duration: 1600, easing: 'ease-out' }"
-        :transform-timing="{ duration: 1600, easing: 'ease-out' }"
-        :opacity-timing="{ duration: 1600, easing: 'ease-out' }"
-        :trend="-1"
-        :plugins="[continuous]" />
-      {{ t("hero.days") }}
+      {{ display }} {{ t("hero.days") }}
     </h1>
     <div
       class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 mt-12 max-w-4xl mx-auto">
       <UCard variant="subtle">
-        <UProgress :model-value="(950 - display) / 9.5" status />
+        <UProgress :model-value="progressBarValue" status />
         <p class="truncate w-full mt-1 text-end text-sm text-primary">
           {{ t("hero.studyProgress") }}
         </p>
       </UCard>
       <UCard variant="subtle">
         <p class="font-medium text-primary text-center text-xl">
-          {{ props.nameDay }}
+          {{ nameDay }}
         </p>
         <p class="text-sm text-dimmed">{{ t("hero.todayNameday") }}</p>
       </UCard>
